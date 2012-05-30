34c3659e0fa21a8f5e2eed020f0ded3d74576f2c
Colin PUY <colin.puy@gmail.com>
2012-05-30 13:44:31 +0200

    tab info

--- src/fr/colin/topoguide/views/TabInfos.java
+++ src/fr/colin/topoguide/views/TabInfos.java
@@ -11,7 +11,7 @@ public class TabInfos extends Activity {
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      setContentView(R.layout.topo_detail_infos);
+      setContentView(R.layout.tab_infos);
       
       TopoGuide topo = (TopoGuide) getIntent().getExtras().getParcelable(getString(R.string.extra_current_topo));
       fillView(topo);
@@ -21,5 +21,14 @@ public class TabInfos extends Activity {
       ((TextView) findViewById(R.id.infos_massif)).setText(topoGuide.sommet.massif);
       ((TextView) findViewById(R.id.infos_secteur)).setText(topoGuide.sommet.secteur);
       ((TextView) findViewById(R.id.infos_orientation)).setText(topoGuide.itineraire.orientation.value());
+      ((TextView) findViewById(R.id.infos_denivele)).setText(String.valueOf(topoGuide.itineraire.denivele) + " m");
+      ((TextView) findViewById(R.id.infos_difficulte_monte)).setText(topoGuide.itineraire.difficulteMontee);
+      ((TextView) findViewById(R.id.infos_difficulte_ski)).setText(topoGuide.itineraire.difficulteSki);
+      ((TextView) findViewById(R.id.infos_nb_jours)).setText(String.valueOf(topoGuide.itineraire.dureeJour));
+      ((TextView) findViewById(R.id.infos_type)).setText(topoGuide.itineraire.type.value());
+      ((TextView) findViewById(R.id.infos_materiel)).setText(topoGuide.itineraire.materiel);
+      ((TextView) findViewById(R.id.infos_depart)).setText(topoGuide.depart.nom);
+      ((TextView) findViewById(R.id.infos_altitude_depart)).setText(String.valueOf(topoGuide.depart.altitude) + " m");
+      ((TextView) findViewById(R.id.infos_pente)).setText(String.valueOf(topoGuide.itineraire.pente));
    }
 }
